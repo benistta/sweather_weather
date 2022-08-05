dac32b991f9dee29d818cd0f184991f1a330d4b9
Becky Nisttahuz <benistta@hotmail.com>
2022-08-05 17:32:51 -0600

    Feat:connection for mapquest establish

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,6 +1,18 @@
 class ForecastService
   class << self
     def get_all_forecast(location)
+    end_point = "/geocoding/v1/address"
+      response = mapquest_connection.get(end_point) do |faraday|
+        faraday.params['key'] = ENV["MAPQUEST_API_KEY"]
+        faraday.params["location"] = location
+      end
+      json = JSON.parse(response.body, symbolize_names: true)
+      require "pry"; binding.pry
+    end
+
+    def mapquest_connection
+      url = "http://www.mapquestapi.com"
+      Faraday.new(url: url)
     end
   end
 end
