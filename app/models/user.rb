5a87a1d2bd83a173850d3c850358b01bb97a5b49
Becky Nisttahuz <benistta@hotmail.com>
2022-08-08 15:49:43 -0600

    Update:model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
+  validates :email, uniqueness: true, presence: true
   validates_presence_of :email
-  validates_uniqueness_of :email
   validates_presence_of :password_digest
 
   before_save :assign_api_key
