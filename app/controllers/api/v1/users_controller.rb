5248379d1aa9277fb2894c2511158d178b0afd5d
Becky Nisttahuz <benistta@hotmail.com>
2022-08-08 15:50:16 -0600

    Feat:Create method user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,19 @@
 class Api::V1::UsersController < ApplicationController
-
   def create
     # require "pry"; binding.pry
-    user = User.create!(user_params)
-    render json: UsersSerializer.new(user), status: 201
+    user = User.new(user_params)
+    if User.find_by(email: params[:email])
+      render json: { data: { message: 'Invalid Email' } }, status: 401
+    elsif params[:password] != params[:password_confirmation]
+      render json: { data: { message: 'Invalid Password' } }, status: 401
+    elsif params[:email].nil? || params[:password].nil? || params[:password_confirmation].nil?
+      render json: { error: "please fill all fields"}, status: :bad_request
+    elsif user.save
+      render json: Api::V1::UsersSerializer.formated_data(user), status: 201
+    end
   end
-#chequear bcryt y hasta que punto chequeean la verificacion de passwords. Does bcrypt takes care of password confirmation automatically
 
+  private
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
